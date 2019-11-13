cfdde5453812115c1a868cdcbba89917f801711a
dannyvidal <dannyjvidal94@gmail.com>
2019-11-13 15:36:09 -0500

    quick fix

--- src/server.js
+++ src/server.js
@@ -10,7 +10,12 @@ import cors from "cors";
 const PORT = process.env.PORT || 5000;
 var app = express();
 
-app.use(cors());
+app.use(function(_, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+ });
 
 app.use(express.json());
 app.use("/api", UserRouter);
