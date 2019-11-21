b53afdd933579dae9f59a0f2cfd4babb6ef36b3c
dannyvidal <dannyjv94@gmail.com>
2019-11-21 15:26:04 -0500

    cors issue

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 var app = express();
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type,token');
     next();
