0b477f0827e2d214988c6a7a376ff6fd5eba769b
dannyvidal <dannyjv94@gmail.com>
2019-11-21 16:18:14 -0500

    cors issue

--- src/server.js
+++ src/server.js
@@ -11,9 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 var app = express();
 
-app.use(cors({credentials:true, origin:"http://localhost:3000", allowedHeaders:{
-    "Access-Control-Expose-Headers":"set-cookie"
-}}) );
+app.use(cors({credentials:true, origin:"http://localhost:3000"}) );
 
 app.use("/api", UserRouter);
 app.use("/api", MemeRouter);
