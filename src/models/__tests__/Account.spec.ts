0dbcc480d1d64bf26527f3905768ebfee0ef55c4
dannyvidal <dannyjv94@gmail.com>
2019-11-23 00:15:40 -0500

    written some tests

--- src/models/__tests__/Account.spec.ts
+++ src/models/__tests__/Account.spec.ts
@@ -1,17 +1,48 @@
-import { expect } from "chai";
+import { expect, assert } from "chai";
 import AccountModel from "../Account";
 import testAccount from "./utils/testAccount";
 import "mocha";
 
-var test = testAccount({ username:"", email:"", password:"" })
-
-console.log(test);
-
-
-// describe("Account Creation", () => {
-//     it("should hash password before creation", async () => {
-//         try{
-//             await AccountModel.create({})
-//         } 
-//     })
-// })
+describe("Valid Account Creation", () => {
+    var _id:string;
+    var test = testAccount({ username:"", email:"", password:"" })
+    test.email = {type:"valid"};
+    test.username = {type:"valid"};
+    test.password = {type:"valid"};
+    //hash is part the excepted AccountModel's Schema, so I am just storing password to a top level variable to not confuse the reader
+    var password:string = (test.hash as string);
+    console.log("running test on ", test);
+    it("successfully gets created", async () => {
+        let account = await AccountModel.create(test);
+        expect(account.toObject()).to.have.own.property("_id");
+        _id = account._id
+    });
+    it("hashes password before creation", async () => {
+        let account = await AccountModel.findById(_id);
+        if(account){
+             //if the hash length is greater then 32 that means its longer then the valid password from the testAccount proxy
+            expect(account.toObject()).to.have.own.property("hash").and.length.to.be.gt(32);
+        }else{
+            assert.fail();
+        }
+    });
+    it("successfully compares with valid password", async () => {
+        let account = await AccountModel.findById(_id);
+        if(account){
+           let compare = await account.compareHash(password);
+           expect(compare).to.equal(true);
+       }else{
+           assert.fail();
+       }
+    });
+    it("successfully compares with invalid password", async () => {
+        let account = await AccountModel.findById(_id);
+        if(account){
+           let compare = await account.compareHash(password + "_");
+           expect(compare).to.equal(false);
+       }else{
+           assert.fail();
+       }
+    })
+         
+})
