e39381257a7d2b48cd1b36f5c0caba556d3ce7ef
dannyvidal <dannyjv94@gmail.com>
2019-11-24 19:58:09 -0500

    added dotenv to top level app entery to use .env

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
+if (process.env.NODE_ENV != "production"){
+    var dotenv = require("dotenv");
+    dotenv.config()
+}
+
+
 const PORT = process.env.PORT || 5000;
-import express from "express";
+import express, { request } from "express";
 import AccountRouter from "./Routes/AccountRouter";
 
 var app = express();
