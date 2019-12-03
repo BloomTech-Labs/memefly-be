8cc923f9381d4d23bd8b447932f16f7534ac1255
dannyvidal <dannyjv94@gmail.com>
2019-12-03 17:41:10 -0500

    changed accountURI to URI

--- src/config/development.ts
+++ src/config/development.ts
@@ -1,5 +1,5 @@
 var dev = {
-    accountURI:"mongodb://localhost:27017/development",
+    URI:"mongodb://localhost:27017/development",
     privateKey:(process.env.PRIVATE_KEY as string),
 }
 export default dev;
