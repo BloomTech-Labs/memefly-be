1ebb25ead585ade15958ab4956c8602993142261
dannyvidal <dannyjv94@gmail.com>
2019-12-03 21:04:16 -0500

    removed dotenv call and added route

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
-if (process.env.NODE_ENV != "production"){
-    var dotenv = require("dotenv");
-    dotenv.config()
-}
 const PORT = process.env.PORT || 5000;
 import express from "express";
 import AccountRouter from "./Routes/AccountRouter";
+import BaseMemesRouter from "./Routes/BaseMemesRouter";
 
 var app = express();
 app.use("/api",AccountRouter);
+app.use("/api", BaseMemesRouter);
 app.listen(PORT, () => {
     console.log(`server up and running *:${PORT}`);
 })
