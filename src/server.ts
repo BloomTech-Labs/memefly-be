92378aad5c11cc14d32d2638db21d6fd52ea971b
dannyvidal <dannyjv94@gmail.com>
2019-12-04 10:53:27 -0500

    added cors

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,10 @@ const PORT = process.env.PORT || 5000;
 import express from "express";
 import AccountRouter from "./Routes/AccountRouter";
 import BaseMemesRouter from "./Routes/BaseMemesRouter";
-
+import cors from "cors"
 var app = express();
 app.use("/api",AccountRouter);
+app.use(cors());
 app.use("/api", BaseMemesRouter);
 app.listen(PORT, () => {
     console.log(`server up and running *:${PORT}`);
